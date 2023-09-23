7daf8024d34c4360344598ced0524c83605f70d0
Gizmo43 <egarus2005@gmail.com>
2023-09-23 21:37:59 +0300

    task with circles

--- practice1_solution/practice1_project/main.c
+++ practice1_solution/practice1_project/main.c
@@ -1,13 +1,46 @@
 #include <stdio.h>
-
+#include <math.h>
+#include <locale.h>
 int main() {
-
+    setlocale(LC_ALL, "Rus");
     int x1, x2, y1, y2, r1, r2;
 
     printf("Input x1,y1,r1: ");
-    scanf("%x1 %y1 %r1",&x1,&y2,&r1);
+    scanf("%d %d %d",&x1,&y1,&r1);
 
+    printf("Input x2,y2,r2: ");
+    scanf("%d %d %d",&x2,&y2,&r2);
+    
+    printf(" \n");
+    if ((x1 == x2) && (y1 == y2) && (r1 == r2))
+        {
+            printf("окружности совпадают\n");
+        }
+    else if (r1 + r2 < sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)))
+        {
+            printf("окружности не пересекаются\n");
+        }
+    else if ((sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))<r1-r2) || (sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) < r2 - r1))
+    {
+        printf("окружности вложены\n");
+    }
+    else if (sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) == r2 + r1)
+    {
+        printf("окружности касаются(внешним образом)\n");
+    }
+    else if ((sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) == r2 - r1) || (sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) == r1 - r2))
+    {
+        printf("окружности касаются(внутренним образом)\n");
+    }
+    else
+    {
+        printf("окружности пересекаются\n");
+    }
 
+    
+    
 
     return 0;
 }
+
+//Формула для расстояния между точками: sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1))
