992ceac4cf7643f9598431d9fcc7ade77bf12ed9
unknown <ITMM-230041@Q113.local>
2023-10-27 13:19:42 +0300

    Fix

--- KrykovEF/Practice2/Practice2/main.c
+++ KrykovEF/Practice2/Practice2/main.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
 #include<locale.h>
 #include<time.h>
-#define RAND_MAX
 
 
 
 int main() {
-    setlocale(LC_ALL, "Rus");
     int flag, n, count,ans,a,b,mid;
     char ret;
+    setlocale(LC_ALL, "Rus");
     printf("Выберите режим игры. 1 - Программа загадывает число или  2 - Программа угадывает число: ");
     scanf("%d",&flag);
     //printf("%d",flag);
@@ -40,11 +39,11 @@ int main() {
             printf("Ваше число %d?", ans);
             scanf("%*c%c", &ret);
             if (ret == '>') {
-                a = ans+1;
+                a = ans + 1;
                 ans = (a + b) / 2;
             }
             if (ret == '<') {
-                b = ans-1;
+                b = ans - 1;
                 ans = (a + b) / 2; 
             }
             count++;
