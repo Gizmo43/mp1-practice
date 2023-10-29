ff096e8db28d10b35573ab6f70152e667d228fbc
Gizmo43 <egarus2005@gmail.com>
2023-10-29 16:33:59 +0300

    Complete

--- KrykovEF/Practice3/Practice3/main.c
+++ KrykovEF/Practice3/Practice3/main.c
@@ -3,70 +3,84 @@
 #include<time.h>
 #define N 5
 
-void main() {
-    int a[N] = { 0,0,0,0,0 }, b[N] = { 0, 0, 0, 0, 0 }, n, i, ans,c,bl,d;
+void dec(int* x, int n,int ans) {
+    int i;
+    for (i = N; i > -1; i--) {
+        if (i > (n - 1))
+            x[i] = 0;
+        else {
+            x[i] = ans % 10;
+            ans = ans / 10;
+        }
+    }
+
+
+}
+
+int main() {
+    int a[N] = { 0,0,0,0,0 }, b[N+1] = { 0, 0, 0, 0, 0, 0 }, n, i,j, ans, cow, bull, d, cnt[10];
     setlocale(LC_ALL, "Rus");
     do {
         printf("Введите длинну загадываемого числа(1-5): ");
         scanf("%d", &n);
     } while ((n > 5) || (n < 1));
 
-    if (n > 0) {
+    for (i = 0; i < 10; i++)
+        cnt[i] = 0;
+    srand((unsigned int)time(0));
+    a[0] = 1 + rand() % 9;
+    cnt[a[0]]++;
+    for (i = 1; i < n; i++) {
         srand((unsigned int)time(0));
-        a[0]=1 + rand() % 9;
-        //printf("%d", a[0]);
+        d = rand() % 10;
+        while(cnt[d]!=0)
+            d = rand() % 10;
+        a[i] = d;
+        cnt[d]++;
     }
-    if(n > 1) {
-        srand((unsigned int)time(0));
-        do {
-            a[1] = rand() % 10;
-        } while (a[1]==a[0]);     
-    }
-    if (n > 2) {
-        srand((unsigned int)time(0));
-        do {
-            a[2] = rand() % 10;
-        } while ((a[2] == a[0]) || (a[2] == a[1]));
-    }
-    if (n > 3) {
-        srand((unsigned int)time(0));
-        do {
-            a[3] = rand() % 10;
-        } while ((a[3] == a[0]) || (a[3] == a[1]) || (a[3] == a[2]));
-    }
-    if (n > 4) {
-        srand((unsigned int)time(0));
-        do {
-            a[4] = rand() % 10;
-        } while ((a[4] == a[0]) || (a[4] == a[1]) || (a[4] == a[2]) || (a[4] == a[3]));
-    }
-    //for (i = 0; i < N; i++) {
-        //printf("%d ", a[i]);        
-    //}
+    /*for (i = 0; i < n; i++)
+        printf("%d ", a[i]);*/
+
     do {
-        printf("Введите число указанной длинны: ");
+        cow = bull = 0;
+        printf("Введите число длинны %d: ",n);
         scanf("%d", &ans);
-        d = 1000;
-        for (i = 0; i < N; i++) {
-            b[i] = ans / d;
-            ans = ans % d;
-            d = d / 10;                    
+        dec(&(b[0]), n, ans);
+        /*for (i = 0; i < n; i++)
+            printf("%d ", b[i]);*/
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++)
+                if (a[i] == b[j])
+                    cow++;
         }
-        for (i = 0; i < N; i++) {
-            printf("%d ", b[i]);        
+        for (i = 0; i < n; i++) {
+            if (a[i] == b[i]) {
+                bull++;
+                cow--;
+            }
+
+            
         }
-
-    } while ((a[0]!=b[0]) && (a[1] != b[1]) && (a[2] != b[2]) && (a[3] != b[3]) && (a[4] != b[4]));
-
+        
 
 
-
-
-
-
+        if ((a[0] == b[0]) && (a[1] == b[1]) && (a[2] == b[2]) && (a[3] == b[3]) && (a[4] == b[4]))
+            break;
+        else
+            printf("Коров: %d Быков: %d\n", cow, bull);
 
 
 
+        
 
-    
+        
+
+    } while ((a[0] != b[0]) || (a[1] != b[1]) || (a[2] != b[2]) || (a[3] != b[3]) || (a[4] != b[4]));
+
+
+
+
+
+    printf("Вы выиграли! Загаданное число: %d", ans);  
+    return 0;
 }
