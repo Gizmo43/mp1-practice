3fe4df19d7bf218da504eac36625aa43f96e01a8
Gizmo43 <egarus2005@gmail.com>
2023-11-10 18:06:03 +0300

    Fix

--- KrykovEF/Practice3/Practice3/main.c
+++ KrykovEF/Practice3/Practice3/main.c
@@ -5,7 +5,7 @@
 
 void dec(int* x, int n,int ans) {
     int i;
-    for (i = N; i > -1; i--) {
+    for (i = N-1; i > -1; i--) {
         if (i > (n - 1))
             x[i] = 0;
         else {
@@ -18,28 +18,25 @@ void dec(int* x, int n,int ans) {
 }
 
 int main() {
-    int a[N] = { 0,0,0,0,0 }, b[N+1] = { 0, 0, 0, 0, 0, 0 }, n, i,j, ans, cow, bull, d, cnt[10];
+    int a[N] = {0}, b[N] = {0}, n, i, j, ans, cow, bull, d, cnt[10] = {0};
     setlocale(LC_ALL, "Rus");
     do {
         printf("Введите длинну загадываемого числа(1-5): ");
         scanf("%d", &n);
     } while ((n > 5) || (n < 1));
 
-    for (i = 0; i < 10; i++)
-        cnt[i] = 0;
     srand((unsigned int)time(0));
     a[0] = 1 + rand() % 9;
     cnt[a[0]]++;
     for (i = 1; i < n; i++) {
-        srand((unsigned int)time(0));
         d = rand() % 10;
         while(cnt[d]!=0)
             d = rand() % 10;
         a[i] = d;
         cnt[d]++;
     }
-    /*for (i = 0; i < n; i++)
-        printf("%d ", a[i]);*/
+    for (i = 0; i < n; i++)
+        printf("%d ", a[i]);
 
     do {
         cow = bull = 0;
@@ -49,38 +46,20 @@ int main() {
         /*for (i = 0; i < n; i++)
             printf("%d ", b[i]);*/
         for (i = 0; i < n; i++) {
-            for (j = 0; j < n; j++)
-                if (a[i] == b[j])
-                    cow++;
-        }
-        for (i = 0; i < n; i++) {
             if (a[i] == b[i]) {
                 bull++;
                 cow--;
             }
-
-            
+            for (j = 0; j < n; j++)
+                if (a[i] == b[j])
+                    cow++;
         }
-        
-
-
-        if ((a[0] == b[0]) && (a[1] == b[1]) && (a[2] == b[2]) && (a[3] == b[3]) && (a[4] == b[4]))
+        if (bull==n)
             break;
         else
-            printf("Коров: %d Быков: %d\n", cow, bull);
+            printf("Коров: %d Быков: %d\n", cow, bull); 
+    } while (bull!=n);
 
-
-
-        
-
-        
-
-    } while ((a[0] != b[0]) || (a[1] != b[1]) || (a[2] != b[2]) || (a[3] != b[3]) || (a[4] != b[4]));
-
-
-
-
-
-    printf("Вы выиграли! Загаданное число: %d", ans);  
+    printf("Вы выиграли! Загаданное число: %d\n", ans);  
     return 0;
 }
