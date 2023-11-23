430e2038422711be5e983fa53289b3fb8491a20e
Gizmo43 <egarus2005@gmail.com>
2023-11-23 22:18:23 +0300

    Fix

--- KrykovEF/Practice4/Practice4/main.c
+++ KrykovEF/Practice4/Practice4/main.c
@@ -6,12 +6,23 @@
 int sum = 0, disc_sum = 0;
 int pr1 = 5000, pr2 = 8000, pr3 = 5000, pr4 = 7000, pr5 = 4000, pr6 = 20000, pr7 = 32000, pr8 = 35000, pr9 = 50000, pr10 = 40000; //цены
 int scan_prod(int* x,int code) {
-    int i = 0;
+    int i = 0, n = 0, k;
+    int code_base[N][4] = { {1,0,0,1},{1,0,0,2},{1,0,0,3},{1,0,0,4},{1,0,0,5},{1,0,0,6},{1,0,0,7},{1,0,0,8},{1,0,0,9},{1,0,1,0}, };
     for (; i < 4; i++) {
-        x[i] = code % 10;
+        x[3-i] = code % 10;
         code = code / 10;
     }
-    return 0;
+    for (n=0; n < N; n++) {
+        k = 0;
+        for (i=0; i < 4; i++) {
+            if (x[i] == code_base[n][i])
+                k++;
+        }
+        if (k == 4)
+            break;
+    }
+
+    return n;
 }
 
 void print_check(int i,int * prod_list) {
@@ -75,7 +86,7 @@ void print_check(int i,int * prod_list) {
 
 void main() {
     
-    int prod_list[N] = {0},pr_code[4],oper,code,n=0,i;
+    int prod_list[N] = {0},pr_code[4],oper,code,n,i;
     setlocale(LC_ALL, "Rus");
 
     do{
@@ -84,85 +95,50 @@ void main() {
         if (oper == 1) {
             printf("Штрихкод(4 цифры): ");
             scanf("%d", &code);
-            scan_prod(&(pr_code[0]), code);
-            //коды
-            switch (code) {
-            case 1001:
-                n = 1;
-                break;
-            case 1002:
-                n = 2;
-                break;
-            case 1003:
-                n = 3;
-                break;
-            case 1004:
-                n = 4;
-                break;
-            case 1005:
-                n = 5;
-            case 1006:
-                n = 6;
-                break;
-            case 1007:
-                n = 7;
-                break;
-            case 1008:
-                n = 8;
-                break;
-            case 1009:
-                n = 9;
-                break;
-            case 1010:
-                n = 10;
-                break;
-            default:
-                printf("Error: no data in base\n");
-            }
-            //номера
+            n=scan_prod(&(pr_code[0]), code);
             switch (n) {
-            case 1:
+            case 0:
                 printf("Шатун 2JZ GE: Цена: %d Скидка: 10%%\n", pr1);
-                prod_list[n - 1]++;
+                prod_list[n]++;
+                break;
+            case 1:
+                printf("Гбц 2JZ GE: Цена: %d Скидка: 5%%\n", pr2);
+                prod_list[n]++;
                 break;
             case 2:
-                printf("Гбц 2JZ GE: Цена: %d Скидка: 5%%\n", pr2);
-                prod_list[n - 1]++;
+                printf("Распредвалы 2jz GE: Цена: %d Скидка: 10%%\n", pr3);
+                prod_list[n]++;
                 break;
             case 3:
-                printf("Распредвалы 2jz GE: Цена: %d Скидка: 10%%\n", pr3);
-                prod_list[n - 1]++;
+                printf("Гбц SR20DE: Цена: %d Скидка: 5%%\n", pr4);
+                prod_list[n]++;
                 break;
             case 4:
-                printf("Гбц SR20DE: Цена: %d Скидка: 5%%\n", pr4);
-                prod_list[n - 1]++;
+                printf("Распредвалы SR20DE: Цена: %d Скидка: 10%%\n", pr5);
+                prod_list[n]++;
                 break;
             case 5:
-                printf("Распредвалы SR20DE: Цена: %d Скидка: 10%%\n", pr5);
-                prod_list[n - 1]++;
+                printf("Турбокомпрессор Garret M24: Цена: %d Скидка: 15%%\n", pr6);
+                prod_list[n]++;
                 break;
             case 6:
-                printf("Турбокомпрессор Garret M24: Цена: %d Скидка: 15%%\n", pr6);
-                prod_list[n - 1]++;
+                printf("Турбокомпрессор Borg Warner S200SX: Цена: %d Скидка: 15%%\n", pr7);
+                prod_list[n]++;
                 break;
             case 7:
-                printf("Турбокомпрессор Borg Warner S200SX: Цена: %d Скидка: 15%%\n", pr7);
-                prod_list[n - 1]++;
+                printf("Турбокомпрессор Holset HE221W: Цена: %d Скидка: 15%%\n", pr8);
+                prod_list[n]++;
                 break;
             case 8:
-                printf("Турбокомпрессор Holset HE221W: Цена: %d Скидка: 15%%\n", pr8);
-                prod_list[n - 1]++;
+                printf("Буд MoTeC MX00 ECUs Цена: %d Скидка: 0%%\n", pr9);
+                prod_list[n]++;
                 break;
             case 9:
-                printf("Буд MoTeC MX00 ECUs Цена: %d Скидка: 0%%\n", pr9);
-                prod_list[n - 1]++;
-                break;
-            case 10:
                 printf("Буд AEM FIC 6 Цена: %d Скидка: 0%%\n", pr10);
-                prod_list[n - 1]++;
+                prod_list[n]++;
                 break;
             default:
-                printf("Error: Empty request\n");
+                printf("Error: No data in base\n");
             }
         }
     } while (oper != 2);
