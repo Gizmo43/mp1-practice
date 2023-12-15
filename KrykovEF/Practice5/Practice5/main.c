16896639a8dd7e7fa88247f97698e7fd3092aaea
Gizmo43 <egarus2005@gmail.com>
2023-12-15 14:28:27 +0300

    Fix

--- KrykovEF/Practice5/Practice5/main.c
+++ KrykovEF/Practice5/Practice5/main.c
@@ -59,8 +59,8 @@ int cmp_size(const void* a, const void* b){
 }
 
 void select_sort(file_data *files, int N) {
-    int min,tmp;
-    char* tmp_n;
+    int min;
+    file_data tmp_s;
     for (int i = 0; i < N; i++)
     {
         min = i;
@@ -68,33 +68,26 @@ void select_sort(file_data *files, int N) {
             if (files[min].size > files[j].size)
                 min = j;
 
-        tmp = files[min].size;
-        files[min].size = files[i].size;
-        files[i].size = tmp;
-
-        tmp_n = files[min].name;
-        files[min].name = files[i].name;
-        files[i].name = tmp_n;
+        tmp_s = files[min];
+        files[min] = files[i];
+        files[i] = tmp_s;
     }
 }
 
 void insert_sort(file_data* files, int N)
 {
-    int new_size,loc;
-    char* new_name;
+    int loc;
+    file_data new_file;
     for (int i = 1; i < N; i++)
     {
-        new_size = files[i].size;
-        new_name = files[i].name;
+        new_file = files[i];
         loc = i - 1;
-        while (loc >= 0 && files[loc].size > new_size)
+        while (loc >= 0 && files[loc].size > new_file.size)
         {
-            files[loc + 1].size = files[loc].size;
-            files[loc + 1].name = files[loc].name;
+            files[loc + 1] = files[loc];
             loc = loc - 1;
         }
-        files[loc + 1].size = new_size;
-        files[loc + 1].name = new_name;
+        files[loc + 1] = new_file;
     }
 }
 
@@ -105,14 +98,50 @@ void print_files(file_data* files, int N) {
 
 }
 
+void qsort_s(file_data* files, int left, int right) {
+    int pilot;
+    char* pilot_name;
 
+    int l_border = left;
+    int r_border = right;
+    pilot = files[left].size;
+    pilot_name = files[left].name;
+    
+    while (left < right) {
+        while ((files[right].size >= pilot) && (left < right))
+            right--;
+        if (left != right) {
+            files[left].size = files[right].size;
+            files[left].name = files[right].name;
+            left++;
+        }
+        while ((files[left].size <= pilot) && (left < right))
+            left++;
+        if (left != right) {
+            files[right].size = files[left].size;
+            files[right].name = files[left].name;
+            right--;
+        }
+    }
+    files[left].size = pilot;
+    files[left].name = pilot_name;
+    pilot = left;
+    left = l_border;
+    right = r_border;
+    if (left < pilot)
+        qsort_s(&files[0], left, pilot - 1);
+    if (right > pilot)
+        qsort_s(&files[0], pilot + 1, right);
+}
 
 
 void main() {
     char path[100];
     int i = 0, num, input = 0;
-    time_t t_start, t_finish;
+    clock_t t_start;
+    clock_t t_finish;
     struct stat fileStat;
+    double time_f;
 
     setlocale(LC_ALL, "Rus");
     printf("Путь: ");
@@ -121,6 +150,7 @@ void main() {
     struct file_data* files_origin = (struct file_data*)malloc(num * sizeof(struct file_data));
     struct file_data* files = (struct file_data*)malloc(num * sizeof(struct file_data));
     //printf("%d\n", num);
+
     d = opendir(path);
     if (d) {
         while ((ent = readdir(d)) != NULL) {
@@ -151,25 +181,30 @@ void main() {
             print_files(&files_origin[0], num);
             break;
         case 1:
-            t_start = time(NULL);
+            t_start = clock();
             select_sort(&files[0], num);
-            t_finish = time(NULL);
-            print_files(&files[0], num);
-            printf("Время сортировки: %lf\n", difftime(t_finish,t_start));
+            print_files(&files[0], num);            
+            t_finish = clock();
+            time_f = (double)((t_finish - t_start)) / CLOCKS_PER_SEC;
+            
+            printf("Время сортировки: %.5lf\n", time_f);
             break;
         case 2:
-            t_start = time(NULL);
+            t_start = clock();
             insert_sort(&files[0], num);
-            t_finish = time(NULL);
             print_files(&files[0], num);
-            printf("Время сортировки: %lf\n", difftime(t_finish, t_start));
+            t_finish = clock();
+            time_f = (double)((t_finish - t_start)) / CLOCKS_PER_SEC;
+            printf("Время сортировки: %.5lf\n", time_f);
             break;
         case 3:
-            t_start = time(NULL);
-            qsort(files, num, sizeof(struct file_data), cmp_size);
-            t_finish = time(NULL);
+            t_start = clock();
+            //qsort(files, num, sizeof(struct file_data), cmp_size);
+            qsort_s(&files[0], 0, num - 1);
             print_files(&files[0], num);
-            printf("Время сортировки: %lf\n", difftime(t_finish, t_start));
+            t_finish = clock();
+            time_f = (double)((t_finish - t_start)) / CLOCKS_PER_SEC;
+            printf("Время сортировки: %.5lf\n", time_f);
             break;
         case 4:
             break;
@@ -177,13 +212,8 @@ void main() {
             printf("Input error\n");
         }
     } while (input != 4);
-
-    
-
-
-
     
     free(files);
 }
 
-//Test path: C:\Users\User\mp1-practice\KrykovEF\Practice5\Practice5
+
