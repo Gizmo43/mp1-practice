d9d1ee2af8d954bca83e3051a78949f7557bbcda
Gizmo43 <egarus2005@gmail.com>
2024-02-25 17:12:33 +0300

    end

--- Krykov/Project1/Project1/main.c
+++ Krykov/Project1/Project1/main.c
@@ -1,80 +1,4 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-
-typedef struct {
-	int n;
-	double* x;
-}TVector;
-
-void alloc(TVector* v, int n) {
-	v->n = n;
-	v->x = (double*)malloc(sizeof(double) * n);
-};
-void fill(TVector* v) {
-	int i = 0;
-	printf("Fill vector: ");
-	for (i; i < v->n; i++) {
-		scanf("%lf", &(v->x[i]));
-	}
-
-}
-void print(TVector* v) {
-	int i = 0;
-	printf("Vector: ");
-	for (i; i < v->n; i++) {
-		printf("%.2lf ", v->x[i]);
-	}
-	printf("\n");
-}
-
-
-TVector plus(TVector* v1, TVector* v2) {
-	TVector res;
-	int i = 0;
-	if (v1->n != v2->n) {
-		printf("v1->n != v2->n");
-		res.x = NULL;
-		res.n = 0;
-		return res;
-	}
-	alloc(&res, v1->n);
-	for (i; i < res.n; i++) {
-		res.x[i] = v1->x[i] + v2->x[i];
-	};
-	return res;
-}
-
-TVector minus(TVector* v1, TVector* v2) {
-	TVector res;
-	int i = 0;
-	if (v1->n != v2->n) {
-		printf("v1->n != v2->n");
-		res.x = NULL;
-		res.n = 0;
-		return res;
-	}
-	alloc(&res, v1->n);
-	for (i; i < res.n; i++) {
-		res.x[i] = v1->x[i] - v2->x[i];
-	};
-	return res;
-}
-double scal(TVector* v1, TVector* v2) {
-	double res=0;
-	int i = 0;
-	if (v1->n != v2->n) {
-		printf("v1->n != v2->n");
-		res = 0;
-		return res;
-	}
-	for (i; i < v1->n; i++) {
-		res = res + v1->x[i] * v2->x[i];
-	}
-
-	return res;
-}
-
+#include "vector.h"
 
 void main() {
 	int n;
@@ -89,14 +13,18 @@ void main() {
 	fill(&v1);
 	fill(&v2);
 
-	/*s = plus(&v1, &v2);
-	print(&s);*/
+	s = plus(&v1, &v2);
+	if (s.x != NULL) {
+		print(&s);
+	}
 
 	s = minus(&v1, &v2);
-	print(&s);
+	if (s.x != NULL) {
+		print(&s);
+	}
 
 	sc = scal(&v1, &v2);
-	printf("%lf", sc);
+	printf("Scal product: %.2lf", sc);
 
 	free(v1.x);
 	free(v2.x);
